2e2039aa15f281301c210ea797c680b44ec78a98
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-08-08 21:30:35 +0530

    Implemented API for autoincrement ID

--- Backend/configs/database.js
+++ Backend/configs/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI  = process.env.MONGO_URI || 'mongodb+srv://Shavidini:sJgeUKZ7waCMGmCG@vehicleregistration.sjbw5wr.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI  = process.env.MONGO_URI || 'mongodb+srv://Shavidini:Sde1234@vehicleregistration.sjbw5wr.mongodb.net/registration';
 
 exports.connect = () => {
   // Connecting to the database
