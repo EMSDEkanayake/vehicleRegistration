e78142f0b04e6f4e5d0b1776c1a2fa46cb5fbd04
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-08-08 20:11:41 +0530

    Added route for add details

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var dotenv = require("dotenv");
 var db = require("./configs/database");
 var path = require('path');
-var cors = require('cors')
+var cors = require('cors');
 
 var vehicleRouter = require('./routes/vehicle');
 
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use('/vehicle',vehicleRouter);
+app.use('/vehicle',vehicleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Backend/routes/vehicle.js
+++ Backend/routes/vehicle.js
@@ -1,2 +1,38 @@
-// var express = require('express');
-// const vehicleModel = require('../models/vehicle');
+var express = require('express');
+const vehicleModel = require('../models/vehicle');
+var router = express.Router();
+const auth = require("../middleware/auth");
+
+//add details
+router.post('/add-details',auth, function (req, res, next) {
+  
+    const bloodbag = new vehicleModel({
+      vehicleId: req.body.vehicleId,
+      vehicleVintage: req.body.vehicleVintage,
+      vehicleOld: req.body.vehicleOld,
+      vehicleModern: req.body.vehicleModern,
+    });
+  
+    try {
+  
+      bloodbag.save();
+      res.status(200).json(
+        {
+          succuss: true,
+          message: 'Insertion succussfull',
+          payload: {}
+        }
+      );
+  
+    }
+    catch (error) {
+      res.status(400).json(
+        {
+          message: 'Cannot add data right now!'
+        }
+      );
+    }
+  
+  });
+
+  module.exports = router;
